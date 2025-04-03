09bd78f815f84640edfa0f01173a27441892c6b4
saleehberlin <berlinsaleeh@gmail.com>
2025-04-03 19:44:40 +0300

    Update models.py

--- aid/models.py
+++ aid/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Aid(models.Model):
+    id=models.AutoField(primary_key=True)
+    brandname=models.CharField(max_length=30)
+    message=models.TextField()
+    donated_at=models.DateTimeField(auto_now_add=True)
+    distributed=models.DateTimeField(auto_now_add=True)
+    website=models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.brandname} {self.donated_at}"
+    
