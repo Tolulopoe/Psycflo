2f77c162d5b9e39d019be5e0f4fd5adf6ab7bd80
saleehberlin <berlinsaleeh@gmail.com>
2025-04-03 19:52:18 +0300

    Update urls.py

--- psycflo/urls.py
+++ psycflo/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/chatbot/', include('chatbot.urls')),  
+    path('api/chatbot/', include('chatbot.urls')), 
+    path('',include ('aid.urls')),
+    
 ]
