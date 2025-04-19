0ff2df7cf8be7359f7361a7d0d457551455da00d
Tolulopoe <treasureddammie@gmail.com>
2025-04-19 15:31:29 +0100

    timestamp

--- psycflo/settings.py
+++ psycflo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', #added these for user authentication
     'rest_framework_simplejwt',
+    'rest_framework.authtoken',
     'users', 
     'community',
     'therapy',
